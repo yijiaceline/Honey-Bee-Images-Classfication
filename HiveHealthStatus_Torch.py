d0297ac1bf69ed95a5f9bed11fcd18aa42047000
Phoebe Wu <phoebe199408@gwu.edu>
2018-12-02 20:11:34 -0500

    torch

--- HiveHealthStatus_Torch.py
+++ HiveHealthStatus_Torch.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
@@ -8,13 +7,11 @@ import torch.optim as optim
 from torch.utils.data import Dataset
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
-import skimage
-import skimage.io
 from pandas import Series
 import os
-import skimage.transform
 from PIL import Image
 from time import time
+import matplotlib.pyplot as plt
 
 
 data_dir = 'input'
@@ -30,37 +27,36 @@ healthy = data.loc[data['health'] == 'healthy']
 data = data.replace(['hive being robbed', 'few varrao, hive beetles', 'Varroa, Small Hive Beetles', 'ant problems', 'missing queen'],
              'unhealthy')
 
+
 def to_file_path(file_name):
     return os.path.join(img_dir, file_name)
 
-
-#set subspecies
+#set health
 target = data['health']
 target = Series.as_matrix(target)
 target_list = set(target)
 target_list = list(target_list)
 
 dic = {}
-for i in range(2):
+for i in range(2): #2 categories
     dic[target_list[i]] = i
 data = data.replace({"health": dic})
 
 #define dataset
-
 class honeybee(Dataset):
-    def __init__(self, data, transform = None):
+    def __init__(self, data):
         self.data = data
         self.img_dir = 'input/bee_imgs'
-        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
     def __getitem__(self, index):
-        img = os.path.join(self.img_dir, data.iloc[index,0])
+        img = os.path.join(self.img_dir, self.data.iloc[index,0])
         image = Image.open(img)
         image = image.resize((120,120))
         image = image.convert('RGB')
         image = self.transform(image)
-        label = self.data.iloc[index, 6]
-        label = torch.tensor(np.asarray(label))
+        label = np.asarray(self.data.iloc[index, 6])
 
         return image,label
 
@@ -69,54 +65,50 @@ class honeybee(Dataset):
 
 
 # split train test
-train_data, test_data = train_test_split(data, test_size=0.3)
-train_data = honeybee(train_data)
-test_data = honeybee(test_data)
-
+train, test = train_test_split(data, test_size=0.3)
+train_data = honeybee(train)
+test_data = honeybee(test)
 
 epochs = 30
 batch_size = 32
-learning_rate = 0.01
+learning_rate = 0.001
 
 #Data Loader
 train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False)
-'''
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-'''
+
 # CNN Model
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 50, kernel_size=3, padding=2),  # RGB image channel = 3, output channel = num_filter
-            nn.BatchNorm2d(50),
-            nn.ReLU())
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=2),  # RGB image channel = 3, output channel = num_filter
+            nn.ReLU(),
+            nn.MaxPool2d(2)) #61
+
         self.layer2 = nn.Sequential(
-            nn.Conv2d(50, 50, kernel_size=3, stride= 1, padding=2),
-            nn.BatchNorm2d(50),
+            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=2), #64
             nn.ReLU(),
-            nn.MaxPool2d(2))
-        '''
+            nn.MaxPool2d(2)) #32
+
         self.layer3 = nn.Sequential(
-            nn.Conv2d(50, 50, kernel_size=3, stride=1, padding=2),
-            nn.BatchNorm2d(50),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        '''
-        self.fc1 = nn.Linear(62*62*50,64)
-        self.fc2 = nn.Linear(64, 2)
+            nn.Conv2d(64, 64, kernel_size=2, stride=1), #31
+            nn.Softmax2d(),
+            nn.MaxPool2d(kernel_size=3, stride=2) #15
+            )
+
+        self.fc1 = nn.Linear(15*15*64,2)
+        #self.fc2 = nn.Linear(64, 2)
+
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        #out = self.layer3(out)
+        out = self.layer3(out)
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
-        out = self.fc2(out)
+        #out = self.fc2(out)
         return out
 
 
@@ -124,11 +116,14 @@ cnn = CNN()
 cnn.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(cnn.parameters(), lr=learning_rate)
+optimizer = optim.Adadelta(cnn.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
 # -----------------------------------------------------------------------------------
 start = time()
+Loss = []
 # Train the Model
 for epoch in range(epochs):
+    scheduler.step()
     for i, (images, labels) in enumerate(train_loader):
         images = Variable(images).cuda()
         labels = Variable(labels).cuda()
@@ -143,11 +138,12 @@ for epoch in range(epochs):
         if (i + 1) % 100 == 0:
             print('Epoch [%d/%d],  Loss: %.4f'
                   % (epoch + 1, epochs, loss.item()))
+        Loss.append(loss.item())
 
 
 # -----------------------------------------------------------------------------------
 # Test the Model
-cnn.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+
 correct = 0
 total = 0
 for images, labels in test_loader:
@@ -160,4 +156,10 @@ for images, labels in test_loader:
 end = time()
 print('Computational Time:', end - start)
 # -----------------------------------------------------------------------------------
-print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+print('Test Accuracy of the model on the 1552 test images: %d %%' % (100 * correct / total))
+
+plt.title("CrossEntropyLoss")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.plot(Loss)
+plt.show()
