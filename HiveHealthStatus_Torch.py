8e9d75b64e92a4ff261eab8cdd920bf4d6e3aff0
Phoebe Wu <phoebe199408@gwu.edu>
2018-12-03 19:15:41 -0500

    Torch

--- HiveHealthStatus_Torch.py
+++ HiveHealthStatus_Torch.py
@@ -12,16 +12,16 @@ import os
 from PIL import Image
 from time import time
 import matplotlib.pyplot as plt
+from sklearn import metrics
 
 
-data_dir = '../input'
+data_dir = 'input'
 img_dir = os.path.join(data_dir, 'bee_imgs')
 data_csv = os.path.join(data_dir, 'bee_data.csv')
 data = pd.read_csv(data_csv)
 
 #unhealthy
 unhealthy = data.loc[data['health'] != 'healthy']
-
 healthy = data.loc[data['health'] == 'healthy']
 
 data = data.replace(['hive being robbed', 'few varrao, hive beetles', 'Varroa, Small Hive Beetles', 'ant problems', 'missing queen'],
@@ -46,7 +46,7 @@ data = data.replace({"health": dic})
 class honeybee(Dataset):
     def __init__(self, data):
         self.data = data
-        self.img_dir = '../input/bee_imgs'
+        self.img_dir = 'input/bee_imgs'
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
@@ -94,8 +94,9 @@ class CNN(nn.Module):
 
         self.layer3 = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=2, stride=1), #31
-            # nn.Softmax2d(),
-            nn.MaxPool2d(kernel_size=3, stride=2) #15
+            nn.MaxPool2d(kernel_size=3, stride=2), #15
+            #add Dropout
+            nn.Dropout(p=0.5)
             )
 
         self.fc1 = nn.Linear(15*15*64,64)
@@ -109,6 +110,7 @@ class CNN(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
         out = self.fc2(out)
+
         return out
 
 
@@ -117,7 +119,7 @@ cnn.cuda()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adadelta(cnn.parameters(), lr=learning_rate)
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
+
 # -----------------------------------------------------------------------------------
 start = time()
 Loss = []
@@ -143,23 +145,66 @@ for epoch in range(epochs):
 
 # -----------------------------------------------------------------------------------
 # Test the Model
+Label = []
+Predict = []
+y_predict = []
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
 
 correct = 0
 total = 0
+
 for images, labels in test_loader:
     images = Variable(images).cuda()
     outputs = cnn(images)
     _, predicted = torch.max(outputs.data, 1)
+    y_predict.append(predicted.cpu().numpy())
+    #get probabilty
+    prob = nn.functional.softmax(outputs, dim=1)
+    Predict.append(prob.detach().cpu().numpy())
+    Label.append(labels.cpu().numpy())
     total += labels.size(0)
     correct += (predicted.cpu() == labels).sum()
+
+pred = np.concatenate(Predict, axis=0)
+y = np.concatenate(Label,axis=0)
+y = pd.get_dummies(y).values
+
+for i in range(len(target_list)):
+    fpr[i], tpr[i], _ = metrics.roc_curve(y[:,i], pred[:,i])
+    roc_auc[i] = metrics.auc(fpr[i],tpr[i])
 
 end = time()
 print('Computational Time:', end - start)
 # -----------------------------------------------------------------------------------
 print('Test Accuracy of the model on the 1552 test images: %d %%' % (100 * correct / total))
 
+#print the f1 score
+Label = np.concatenate(Label).ravel()
+y_predict = np.concatenate(y_predict).ravel()
+print(metrics.classification_report(Label, y_predict, target_names=target_list))
+
+#Plot the loss
 plt.title("CrossEntropyLoss")
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.plot(Loss)
+plt.ylim(0,1)
 plt.show()
+
+#Plot the ROC curve
+colors = (['blue', 'red'])
+for i, color in zip(range(2), colors):
+    plt.plot(fpr[i], tpr[i], color=color,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+plt.plot([0, 1], [0, 1], 'k--',)
+plt.xlim([0, 1])
+plt.ylim([0,1])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic for all labels')
+plt.legend(loc="lower right")
+plt.show()
+
