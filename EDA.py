52ae10989f4112ba3c97f742d5f23aa99e6bbe95
Phoebe Wu <phoebe199408@gwu.edu>
2018-12-03 21:25:47 -0500

    EDA.py

---
+++ EDA.py
@@ -0,0 +1,75 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+bees = pd.read_csv('/Users/phoebewu/Desktop/final/honey-bee-annotated-images/bee_data.csv')
+bees.head()
+
+#check null value
+bees.isnull().sum() 
+
+#check duplicates
+duplicates = bees.duplicated()
+duplicates[duplicates > 1]
+duplicates = bees.file.value_counts()
+duplicates[duplicates > 1]
+
+plt.figure(figsize=(8,8))
+bees.date.value_counts().plot(kind = 'bar')
+plt.title('Date')
+plt.show()
+
+plt.figure(figsize=(8,8))
+bees.time.value_counts().plot(kind = 'bar')
+plt.title('Time')
+plt.show()
+
+plt.figure(figsize=(6,6))
+bees.location.value_counts().plot(kind = 'pie')
+plt.title('Location')
+plt.show()
+
+bees['subspecies'].value_counts()
+
+plt.figure(figsize=(6,6))
+bees.subspecies.value_counts().plot(kind = 'pie')
+plt.title('Subspecises')
+plt.show()
+
+bees['health'].value_counts()
+
+plt.figure(figsize=(6,6))
+bees.health.value_counts().plot(kind = 'pie')
+plt.title('Hive Health')
+plt.show()
+
+plt.figure(figsize=(6,6))
+bees.pollen_carrying.value_counts().plot(kind = 'bar')
+plt.title('Pollen Carrying')
+plt.show()
+
+plt.figure(figsize=(6,6))
+bees.caste.value_counts().plot(kind = 'bar')
+plt.title('Caste')
+plt.show()
+
+unhealthy = bees.loc[bees['health'] != 'healthy']
+#unhealthy
+unhealthy.count()
+
+healthy = bees.loc[bees['health'] == 'healthy']
+healthy.count()
+
+bees.health.value_counts()
+
+New = bees.replace(['hive being robbed', 'few varrao, hive beetles', 'Varroa, Small Hive Beetles', 'ant problems', 'missing queen'],
+             'unhealthy')
+
+New = bees.replace(['hive being robbed', 'few varrao, hive beetles', 'Varroa, Small Hive Beetles', 'ant problems', 'missing queen'],
+             'unhealthy')
+
+plt.figure(figsize=(6,6))
+New.health.value_counts().plot(kind = 'pie')
+plt.title('Hive Health')
+plt.show()
+
